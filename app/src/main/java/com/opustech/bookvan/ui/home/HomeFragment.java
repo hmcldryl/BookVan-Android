855175aeb237647d5c519b4203a737e4a0d873c2
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-08 05:09:45 +0800

    Update HomeFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/home/HomeFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/home/HomeFragment.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,7 +19,11 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.opustech.bookvan.MainActivity;
 import com.opustech.bookvan.R;
+import com.opustech.bookvan.ui.book.BookFragment;
+import com.opustech.bookvan.ui.rent.RentFragment;
 
 import org.imaginativeworld.whynotimagecarousel.CarouselItem;
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
@@ -28,6 +33,10 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private CardView btnBooking, btnRental, btnPartner;
+
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -36,8 +45,36 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        btnBooking = root.findViewById(R.id.btnBooking);
+        btnRental = root.findViewById(R.id.btnRental);
+        btnPartner = root.findViewById(R.id.btnPartner);
+
         ImageCarousel imageCarousel = root.findViewById(R.id.carousel);
         List<CarouselItem> list = new ArrayList<>();
+
+        btnBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    ((MainActivity)getActivity()).openLoginDialog();
+                }
+                else {
+                    ((MainActivity)getActivity()).replaceFragment(BookFragment.class);
+                }
+            }
+        });
+
+        btnRental.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    ((MainActivity)getActivity()).openLoginDialog();
+                }
+                else {
+                    ((MainActivity)getActivity()).replaceFragment(RentFragment.class);
+                }
+            }
+        });
 
         return root;
     }
