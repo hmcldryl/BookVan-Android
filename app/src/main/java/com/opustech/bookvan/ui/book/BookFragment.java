a7ccde6c45599c97cae70cc1e97db8f1c562f1f4
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-02-07 17:46:01 +0800

    Update BookFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/book/BookFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/book/BookFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,17 +14,37 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.opustech.bookvan.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BookFragment extends Fragment {
 
     private BookViewModel bookViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         bookViewModel =
                 new ViewModelProvider(this).get(BookViewModel.class);
         View root = inflater.inflate(R.layout.fragment_book, container, false);
+
+        TextInputLayout bookingLocationFrom = root.findViewById(R.id.bookingLocationFrom);
+        TextInputLayout bookingLocationTo = root.findViewById(R.id.bookingLocationTo);
+        AutoCompleteTextView bookingLocationFromACT = root.findViewById(R.id.bookingLocationFromACT);
+        AutoCompleteTextView bookingLocationToACT = root.findViewById(R.id.bookingLocationToACT);
+
+        ArrayList<String> locationArray = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.destinations)));
+        ArrayAdapter<String> locationArrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, locationArray);
+
+        bookingLocationFromACT.setAdapter(locationArrayAdapter);
+        bookingLocationFromACT.setThreshold(1);
+        bookingLocationToACT.setAdapter(locationArrayAdapter);
+        bookingLocationToACT.setThreshold(1);
+
         return root;
     }
 }
