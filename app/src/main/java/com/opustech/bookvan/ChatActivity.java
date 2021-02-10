108c8e9e97856d9cec40aa74b1655a0bf5332466
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-02-10 19:35:41 +0800

    Create ChatActivity.java

---
+++ app/src/main/java/com/opustech/bookvan/ChatActivity.java
@@ -0,0 +1,30 @@
+package com.opustech.bookvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setTitle("");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
