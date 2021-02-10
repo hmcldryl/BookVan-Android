036b5d176796360f4cabb7002394114024a96a26
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-10 11:47:18 +0800

    Update BookViewModel.java

--- app/src/main/java/com/opustech/bookvan/ui/book/BookViewModel.java
+++ app/src/main/java/com/opustech/bookvan/ui/book/BookViewModel.java
@@ -4,7 +4,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class BookViewModel extends ViewModel {
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private CollectionReference usersReference = firebaseFirestore.collection("users");
 
     private MutableLiveData<String> mText;
 
