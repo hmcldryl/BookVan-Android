ae1c22f94a3b24c05e305363ddfaaf29287f1d6a
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-10 11:50:09 +0800

    Update ProfileFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/profile/ProfileFragment.java
@@ -8,10 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+import com.opustech.bookvan.ProfileViewPagerAdapter;
 import com.opustech.bookvan.R;
 
 public class ProfileFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private ProfileViewPagerAdapter profileViewPagerAdapter;
 
     private ProfileViewModel profileViewModel;
 
@@ -20,6 +28,13 @@ public class ProfileFragment extends Fragment {
         profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        viewPager = root.findViewById(R.id.profileViewPager);
+        tabLayout = root.findViewById(R.id.profileTab);
+        profileViewPagerAdapter = new ProfileViewPagerAdapter(getParentFragmentManager());
+        viewPager.setAdapter(profileViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
         return root;
     }
 }
