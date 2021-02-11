02f3e945f14c3aa4c2197f361eb384588f65fea4
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-11 18:34:06 +0800

    Create ScheduleAdminFragment.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/ScheduleAdminFragment.java
@@ -0,0 +1,21 @@
+package com.opustech.bookvan.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.opustech.bookvan.R;
+
+public class ScheduleAdminFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_schedule, container, false);
+        return root;
+    }
+}
