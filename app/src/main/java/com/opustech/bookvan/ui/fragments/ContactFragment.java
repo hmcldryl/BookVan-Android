545a6dffd806f55f12e2a3d4f7610c37b5bdd542
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-11 18:33:42 +0800

    Create ContactFragment.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/ContactFragment.java
@@ -0,0 +1,18 @@
+package com.opustech.bookvan.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.opustech.bookvan.R;
+
+public class ContactFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_contact, container, false);
+        return root;
+    }
+}
