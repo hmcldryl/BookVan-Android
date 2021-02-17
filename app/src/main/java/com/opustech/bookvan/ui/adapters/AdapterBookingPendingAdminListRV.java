380d07b9e9b7f339e3705d6f203f452545f1b589
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-18 00:40:57 +0800

    Update AdapterBookingPendingAdminListRV.java

--- app/src/main/java/com/opustech/bookvan/ui/adapters/AdapterBookingPendingAdminListRV.java
+++ app/src/main/java/com/opustech/bookvan/ui/adapters/AdapterBookingPendingAdminListRV.java
@@ -1,6 +1,7 @@
 package com.opustech.bookvan.ui.adapters;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
@@ -22,6 +23,7 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -58,19 +60,19 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
         firebaseFirestore = FirebaseFirestore.getInstance();
         usersReference = firebaseFirestore.collection("users");
 
-        String customerId = model.getUid();
-        String customerName = model.getName();
-        String bookingContactNumber = model.getContact_number();
-        String bookingReferenceNumber = model.getReference_number();
-        String bookingLocationFrom = model.getLocation_from();
-        String bookingLocationTo = model.getLocation_to();
-        String bookingScheduleDate = model.getSchedule_date();
-        String bookingScheduleTime = model.getSchedule_time();
-        String bookingCountAdult = model.getCount_adult();
-        String bookingCountChild = model.getCount_child();
-        String checkoutTotal = model.getPrice();
+        String uid = model.getUid();
+        String name = model.getName();
+        String contact_number = model.getContact_number();
+        String reference_number = model.getReference_number();
+        String location_from = model.getLocation_from();
+        String location_to = model.getLocation_to();
+        String schedule_date = model.getSchedule_date();
+        String schedule_time = model.getSchedule_time();
+        int count_adult = model.getCount_adult();
+        int count_child = model.getCount_child();
+        float price = model.getPrice();
 
-        usersReference.document(customerId)
+        usersReference.document(uid)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -86,16 +88,16 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
                     }
                 });
 
-        holder.bookingCustomerName.setText(customerName);
-        holder.bookingContactNumber.setText(bookingContactNumber);
-        holder.bookingReferenceNumber.setText(bookingReferenceNumber);
-        holder.bookingLocationFrom.setText(bookingLocationFrom);
-        holder.bookingLocationTo.setText(bookingLocationTo);
-        holder.bookingScheduleDate.setText(bookingScheduleDate);
-        holder.bookingScheduleTime.setText(bookingScheduleTime);
-        holder.bookingCountAdult.setText(bookingCountAdult);
-        holder.bookingCountChild.setText(bookingCountChild);
-        holder.bookingPrice.setText(checkoutTotal);
+        holder.bookingCustomerName.setText(name);
+        holder.bookingContactNumber.setText(contact_number);
+        holder.bookingReferenceNumber.setText(reference_number);
+        holder.bookingLocationFrom.setText(location_from);
+        holder.bookingLocationTo.setText(location_to);
+        holder.bookingScheduleDate.setText(schedule_date);
+        holder.bookingScheduleTime.setText(schedule_time);
+        holder.bookingCountAdult.setText(count_adult);
+        holder.bookingCountChild.setText(count_child);
+        holder.bookingPrice.setText((int) price);
 
         holder.bookingCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,7 +119,7 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
                     TextInputLayout inputDriverName = dialogView.findViewById(R.id.inputDriverName);
                     TextInputLayout inputVanPlate = dialogView.findViewById(R.id.inputVanPlate);
 
-                    String customerNameId = "for " + customerName + " (" + bookingReferenceNumber + ")";
+                    String customerNameId = "for " + name + " (" + reference_number + ")";
                     bookingCustomerNameId.setText(customerNameId);
 
                     MaterialButton btnCancelBooking = dialogView.findViewById(R.id.btnCancelBooking);
@@ -129,7 +131,24 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
                     btnCancelBooking.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            alertDialog.dismiss();
+                            new MaterialAlertDialogBuilder(holder.itemView.getContext())
+                                    .setTitle("Cancel this booking?")
+                                    .setMessage("Note that this action will also delete the booking from your system.")
+                                    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            dialogInterface.dismiss();
+                                            alertDialog.dismiss();
+                                        }
+                                    })
+                                    .setPositiveButton("Cancel Booking", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            dialogInterface.dismiss();
+                                            alertDialog.dismiss();
+                                        }
+                                    })
+                                    .show();
                         }
                     });
 
@@ -162,8 +181,16 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
                                     public void onComplete(@NonNull Task<Void> task) {
                                         if (task.isSuccessful()) {
                                             btnConfirmBooking.setEnabled(true);
-                                            alertDialog.dismiss();
-                                            Toast.makeText(v.getContext(), "Successfully confirmed this booking.", Toast.LENGTH_SHORT).show();
+                                            new MaterialAlertDialogBuilder(holder.itemView.getContext())
+                                                    .setTitle("Successfully confirmed this booking.")
+                                                    .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                                            dialogInterface.dismiss();
+                                                            alertDialog.dismiss();
+                                                        }
+                                                    })
+                                                    .show();
                                         }
                                     }
                                 });
@@ -216,7 +243,7 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
             bookingScheduleTime = view.findViewById(R.id.bookingScheduleTime);
             bookingCountAdult = view.findViewById(R.id.bookingCountAdult);
             bookingCountChild = view.findViewById(R.id.bookingCountChild);
-            bookingPrice = view.findViewById(R.id.checkoutTotal);
+            bookingPrice = view.findViewById(R.id.price);
         }
     }
 
