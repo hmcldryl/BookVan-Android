e8b7f361e34a3b15f273f943b0b4e8600a7dc937
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-17 23:17:07 +0800

    Update ProfileBookingHistoryFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/fragments/user/profile/ProfileBookingHistoryFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/user/profile/ProfileBookingHistoryFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,8 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.opustech.bookvan.R;
 import com.opustech.bookvan.model.Booking;
 import com.opustech.bookvan.ui.adapters.AdapterBookingHistoryListRV;
@@ -46,7 +50,7 @@ public class ProfileBookingHistoryFragment extends Fragment {
         Query confirmedBookingQuery = usersReference.document(admin_uid)
                 .collection("bookings")
                 .whereEqualTo("uid", currentUserId)
-                .whereEqualTo("status", "confirmed")
+                .whereEqualTo("status", "done")
                 .orderBy("timestamp", Query.Direction.DESCENDING);
 
         FirestoreRecyclerOptions<Booking> historyBookingOptions = new FirestoreRecyclerOptions.Builder<Booking>()
@@ -63,6 +67,25 @@ public class ProfileBookingHistoryFragment extends Fragment {
         bookingList.setHasFixedSize(true);
         bookingList.setLayoutManager(historyBookingListManager);
         bookingList.setAdapter(adapterHistoryBookingListRV);
+
+        usersReference.document(admin_uid)
+                .collection("bookings")
+                .whereEqualTo("uid", currentUserId)
+                .whereEqualTo("status", "done")
+                .addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        int size = value.size();
+
+                        if (size > 0) {
+                            bookingList.setVisibility(View.VISIBLE);
+                            bookingStatusNone.setVisibility(View.GONE);
+                        } else {
+                            bookingStatusNone.setVisibility(View.VISIBLE);
+                            bookingList.setVisibility(View.GONE);
+                        }
+                    }
+                });
 
         return root;
     }
