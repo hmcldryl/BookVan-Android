fe0f32172b2d4c07dddfea68342289347906a716
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-17 23:15:26 +0800

    Update ChatMessage.java

--- app/src/main/java/com/opustech/bookvan/model/ChatMessage.java
+++ app/src/main/java/com/opustech/bookvan/model/ChatMessage.java
@@ -1,15 +1,18 @@
 package com.opustech.bookvan.model;
 
+import com.google.firebase.Timestamp;
+
 public class ChatMessage {
-    String message, timestamp, uid;
+    String message, uid;
+    Timestamp timestamp;
 
     public ChatMessage() {
     }
 
-    public ChatMessage(String message, String timestamp, String uid) {
+    public ChatMessage(String message, String uid, Timestamp timestamp) {
         this.message = message;
-        this.timestamp = timestamp;
         this.uid = uid;
+        this.timestamp = timestamp;
     }
 
     public String getMessage() {
@@ -20,19 +23,19 @@ public class ChatMessage {
         this.message = message;
     }
 
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
     public String getUid() {
         return uid;
     }
 
     public void setUid(String uid) {
         this.uid = uid;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
     }
 }
