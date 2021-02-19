37bb7cf82d69ecb31b47fe30dfb7694422eeab70
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-20 01:11:57 +0800

    Create BookingFragment.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/user/booking/BookingFragment.java
@@ -0,0 +1,146 @@
+package com.opustech.bookvan.ui.fragments.user.booking;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.opustech.bookvan.ui.adapters.ProfilePagerAdapter;
+import com.opustech.bookvan.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+
+import cc.cloudist.acplibrary.ACProgressConstant;
+import cc.cloudist.acplibrary.ACProgressFlower;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.*;
+
+public class BookingFragment extends Fragment {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference usersReference;
+    private StorageReference storageReference;
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+
+    private ProfilePagerAdapter profilePagerAdapter;
+
+    private String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_booking, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        usersReference = firebaseFirestore.collection("users");
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        String currentUserId = firebaseAuth.getCurrentUser().getUid();
+
+        viewPager = root.findViewById(R.id.profileViewPager);
+        tabLayout = root.findViewById(R.id.profileTab);
+
+        profilePagerAdapter = new ProfilePagerAdapter(getActivity());
+        viewPager.setAdapter(profilePagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("Active Booking");
+                        updateActiveListTabBadge(tab, currentUserId);
+                        break;
+                    case 1:
+                        tab.setText("Booking History");
+                        updateHistoryListTabBadge(tab, currentUserId);
+                        break;
+                }
+            }
+        }).attach();
+
+        return root;
+    }
+
+    private void updateActiveListTabBadge(TabLayout.Tab tab, String uid) {
+        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+        badgeDrawable.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorBadgeBackground));
+        badgeDrawable.setMaxCharacterCount(2);
+        usersReference.document(admin_uid)
+                .collection("bookings")
+                .whereEqualTo("uid", uid)
+                .whereEqualTo("status", "pending")
+                .whereEqualTo("status", "confirmed")
+                .addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value != null) {
+                            int size = value.size();
+                            if (size > 0) {
+                                BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                                badgeDrawable.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorBadgeBackground));
+                                badgeDrawable.setMaxCharacterCount(2);
+                                badgeDrawable.setNumber(size);
+                                badgeDrawable.setVisible(true);
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void updateHistoryListTabBadge(TabLayout.Tab tab, String uid) {
+        usersReference.document(admin_uid)
+                .collection("bookings")
+                .whereEqualTo("uid", uid)
+                .whereEqualTo("status", "done")
+                .addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value != null) {
+                            int size = value.size();
+                            if (size > 0) {
+                                BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                                badgeDrawable.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorBadgeBackground));
+                                badgeDrawable.setMaxCharacterCount(2);
+                                badgeDrawable.setNumber(size);
+                                badgeDrawable.setVisible(true);
+                            }
+                        }
+                    }
+                });
+    }
+}
