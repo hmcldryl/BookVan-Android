b5b798bf22fdc238a39c41f36f6701ee4e125757
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-20 19:21:53 +0800

    Update AdapterBookingPendingListRV.java

--- app/src/main/java/com/opustech/bookvan/ui/adapters/AdapterBookingPendingListRV.java
+++ app/src/main/java/com/opustech/bookvan/ui/adapters/AdapterBookingPendingListRV.java
@@ -1,44 +1,24 @@
 package com.opustech.bookvan.ui.adapters;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.opustech.bookvan.R;
 import com.opustech.bookvan.model.Booking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
-import cc.cloudist.acplibrary.ACProgressConstant;
-import cc.cloudist.acplibrary.ACProgressFlower;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AdapterBookingPendingListRV extends FirestoreRecyclerAdapter<Booking, AdapterBookingPendingListRV.BookingHolder> {
@@ -85,9 +65,11 @@ public class AdapterBookingPendingListRV extends FirestoreRecyclerAdapter<Bookin
                             String customerEmail = task.getResult().getString("email");
                             holder.bookingCustomerEmail.setText(customerEmail);
                             String customerPhoto = task.getResult().getString("photo_url");
-                            Glide.with(holder.itemView.getContext())
-                                    .load(customerPhoto)
-                                    .into(holder.customerPhoto);
+                            if (customerPhoto != null) {
+                                Glide.with(holder.itemView.getContext())
+                                        .load(customerPhoto)
+                                        .into(holder.customerPhoto);
+                            }
                         }
                     }
                 });
