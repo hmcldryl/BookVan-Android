0fc575d7655da29e6ace2a410455b0faf9103dfd
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-20 19:26:40 +0800

    Update RentalsFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/fragments/admin/rentals/RentalsFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/admin/rentals/RentalsFragment.java
@@ -6,29 +6,65 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.opustech.bookvan.R;
-import com.opustech.bookvan.ui.adapters.RentalsViewPagerAdapter;
+import com.opustech.bookvan.ui.adapters.ProfilePagerAdapter;
+import com.opustech.bookvan.ui.adapters.RentalsPagerAdapter;
 
 public class RentalsFragment extends Fragment {
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference usersReference;
 
-    private RentalsViewPagerAdapter rentalsViewPagerAdapter;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+
+    private RentalsPagerAdapter rentalsPagerAdapter;
+
+    private String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_rentals, container, false);
 
-        viewPager = root.findViewById(R.id.rentalsViewPager);
-        tabLayout = root.findViewById(R.id.rentalsTab);
-        rentalsViewPagerAdapter = new RentalsViewPagerAdapter(getParentFragmentManager());
-        viewPager.setAdapter(rentalsViewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        usersReference = firebaseFirestore.collection("users");
+
+        viewPager = root.findViewById(R.id.viewPager);
+        tabLayout = root.findViewById(R.id.tabLayout);
+
+        rentalsPagerAdapter = new RentalsPagerAdapter(getActivity());
+        viewPager.setAdapter(rentalsPagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("Rental Form");
+                        break;
+                    case 1:
+                        tab.setText("Van Listings");
+                        break;
+                }
+            }
+        }).attach();
 
         return root;
     }
