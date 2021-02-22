136565b14585a4fcc51d1a90a9f9828798f607c6
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-22 18:18:47 +0800

    Create User.java

---
+++ app/src/main/java/com/opustech/bookvan/model/User.java
@@ -0,0 +1,10 @@
+package com.opustech.bookvan.model;
+
+public class User {
+    String name,
+            email,
+            contact_number,
+            photo_url,
+            account_type,
+            transport_company;
+}
