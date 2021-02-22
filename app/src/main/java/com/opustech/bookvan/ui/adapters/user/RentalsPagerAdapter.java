da0bd0bc20025ccdfdaaf2cc173b91c88dc605d7
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-22 18:22:50 +0800

    Create RentalsPagerAdapter.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/adapters/user/RentalsPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.opustech.bookvan.ui.adapters.user;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.opustech.bookvan.ui.fragments.admin.rentals.RentalsFormFragment;
+import com.opustech.bookvan.ui.fragments.admin.rentals.RentalsListingFragment;
+
+public class RentalsPagerAdapter extends FragmentStateAdapter {
+
+    public RentalsPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new RentalsFormFragment();
+            default:
+                return new RentalsListingFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
