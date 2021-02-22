e09184c258f86477a6467892f782d11940dbdd77
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-22 18:21:01 +0800

    Create AdapterBookingHistoryAdminListRV.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/adapters/admin/AdapterBookingHistoryAdminListRV.java
@@ -0,0 +1,144 @@
+package com.opustech.bookvan.ui.adapters.admin;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.opustech.bookvan.R;
+import com.opustech.bookvan.model.Booking;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AdapterBookingHistoryAdminListRV extends FirestoreRecyclerAdapter<Booking, AdapterBookingHistoryAdminListRV.BookingHolder> {
+
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference usersReference;
+
+    private String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+
+    public AdapterBookingHistoryAdminListRV(@NonNull FirestoreRecyclerOptions<Booking> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull BookingHolder holder, int position, @NonNull Booking model) {
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        usersReference = firebaseFirestore.collection("users");
+
+        String uid = model.getUid();
+        String name = model.getName();
+        String contact_number = model.getContact_number();
+        String reference_number = model.getReference_number();
+        String location_from = model.getLocation_from();
+        String location_to = model.getLocation_to();
+        String schedule_date = model.getSchedule_date();
+        String schedule_time = model.getSchedule_time();
+        int count_adult = model.getCount_adult();
+        int count_child = model.getCount_child();
+        String transport_name;
+        String driver_name;
+        String plate_number;
+        float price = model.getPrice();
+
+        usersReference.document(uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String customerEmail = task.getResult().getString("email");
+                            holder.bookingCustomerEmail.setText(customerEmail);
+                            String customerPhoto = task.getResult().getString("photo_url");
+                            if (customerPhoto != null) {
+                                Glide.with(holder.itemView.getContext())
+                                        .load(customerPhoto)
+                                        .into(holder.customerPhoto);
+                            }
+                        }
+                    }
+                });
+
+        holder.bookingCustomerName.setText(name);
+        holder.bookingContactNumber.setText(contact_number);
+        holder.bookingReferenceNumber.setText(reference_number);
+        holder.bookingLocationFrom.setText(location_from);
+        holder.bookingLocationTo.setText(location_to);
+        holder.bookingScheduleDate.setText(schedule_date);
+        holder.bookingScheduleTime.setText(schedule_time);
+        holder.bookingCountAdult.setText(String.valueOf(count_adult));
+        holder.bookingCountChild.setText(String.valueOf(count_child));
+        holder.bookingPrice.setText(String.valueOf(price));
+
+    }
+
+    @NonNull
+    @Override
+    public BookingHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_admin_history_item_layout, parent, false);
+        return new BookingHolder(view);
+    }
+
+    class BookingHolder extends RecyclerView.ViewHolder {
+        TextView bookingCustomerName,
+                bookingCustomerEmail,
+                bookingContactNumber,
+                bookingReferenceNumber,
+                bookingLocationFrom,
+                bookingLocationTo,
+                bookingScheduleDate,
+                bookingScheduleTime,
+                bookingCountAdult,
+                bookingCountChild,
+                bookingTransportName,
+                bookingDriverName,
+                bookingPlateNumber,
+                bookingPrice;
+        LinearLayout item;
+        Button btnCancelBooking, btnConfirmBooking;
+        CircleImageView customerPhoto;
+
+        public BookingHolder(View view) {
+            super(view);
+            item = view.findViewById(R.id.item);
+            customerPhoto = view.findViewById(R.id.customerPhoto);
+            btnCancelBooking = view.findViewById(R.id.btnCancelBooking);
+            btnConfirmBooking = view.findViewById(R.id.btnConfirmBooking);
+            bookingCustomerName = view.findViewById(R.id.bookingCustomerName);
+            bookingCustomerEmail = view.findViewById(R.id.bookingCustomerEmail);
+            bookingContactNumber = view.findViewById(R.id.bookingContactNumber);
+            bookingReferenceNumber = view.findViewById(R.id.bookingReferenceNumber);
+            bookingLocationFrom = view.findViewById(R.id.bookingLocationFrom);
+            bookingLocationTo = view.findViewById(R.id.bookingLocationTo);
+            bookingScheduleDate = view.findViewById(R.id.bookingScheduleDate);
+            bookingScheduleTime = view.findViewById(R.id.bookingScheduleTime);
+            bookingCountAdult = view.findViewById(R.id.bookingCountAdult);
+            bookingCountChild = view.findViewById(R.id.bookingCountChild);
+            bookingTransportName = view.findViewById(R.id.bookingTransportName);
+            bookingDriverName = view.findViewById(R.id.bookingDriverName);
+            bookingPlateNumber = view.findViewById(R.id.bookingPlateNumber);
+            bookingPrice = view.findViewById(R.id.price);
+        }
+    }
+}
