2e38f32f58d5b5399e29bca31d042cdc25322a08
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-23 12:33:50 +0800

    Update TransportLoginActivity.java

--- app/src/main/java/com/opustech/bookvan/transport/TransportLoginActivity.java
+++ app/src/main/java/com/opustech/bookvan/transport/TransportLoginActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,16 +25,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.opustech.bookvan.MainActivity;
 import com.opustech.bookvan.R;
+import com.opustech.bookvan.RegisterActivity;
 import com.opustech.bookvan.admin.AdminActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import cc.cloudist.acplibrary.ACProgressConstant;
+import cc.cloudist.acplibrary.ACProgressFlower;
+
 public class TransportLoginActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
-    private CollectionReference usersReference;
+    private CollectionReference usersReference, partnersReference;
 
     private TextInputLayout inputTransportName, inputAccountType, inputEmail, inputPassword;
     private MaterialButton btnLogin;
@@ -46,6 +52,7 @@ public class TransportLoginActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
         usersReference = firebaseFirestore.collection("users");
+        partnersReference = firebaseFirestore.collection("partners");
 
         inputTransportName = findViewById(R.id.inputTransportName);
         inputAccountType = findViewById(R.id.inputAccountType);
@@ -63,13 +70,19 @@ public class TransportLoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final ACProgressFlower dialog = new ACProgressFlower.Builder(TransportLoginActivity.this)
+                        .direction(ACProgressConstant.DIRECT_CLOCKWISE)
+                        .themeColor(getResources().getColor(R.color.white))
+                        .text("Processing...")
+                        .fadeColor(Color.DKGRAY).build();
+                dialog.show();
                 disableInput();
-                inputCheck();
+                inputCheck(dialog);
             }
         });
     }
 
-    private void inputCheck() {
+    private void inputCheck(ACProgressFlower dialog) {
         String transport_name = inputTransportName.getEditText().getText().toString();
         String account_type = inputAccountType.getEditText().getText().toString();
         String email = inputEmail.getEditText().getText().toString();
@@ -77,18 +90,22 @@ public class TransportLoginActivity extends AppCompatActivity {
 
         if (transport_name.isEmpty()) {
             enableInput();
+            dialog.dismiss();
             inputTransportName.setError("Please select your transport company.");
         } else if (account_type.isEmpty()) {
             enableInput();
+            dialog.dismiss();
             inputAccountType.setError("Please select your account type.");
         } else if (email.isEmpty()) {
             enableInput();
+            dialog.dismiss();
             inputEmail.setError("Please enter your email.");
         } else if (password.isEmpty()) {
             enableInput();
+            dialog.dismiss();
             inputEmail.setError("Please select your transport company.");
         } else {
-            onLogin(transport_name, account_type, email, password);
+            onLogin(dialog, transport_name, account_type, email, password);
         }
     }
 
@@ -108,19 +125,23 @@ public class TransportLoginActivity extends AppCompatActivity {
         inputPassword.setEnabled(true);
     }
 
-    private void onLogin(String transport_name, String account_type, String email, String password) {
+    private void onLogin(ACProgressFlower dialog, String transport_name, String account_type, String email, String password) {
         firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            checkUserSession(transport_name, account_type);
+                            checkUserSession(dialog, transport_name, account_type);
+                        }
+                        else {
+                            dialog.dismiss();
+                            Toast.makeText(TransportLoginActivity.this, "Sign in failed. Please check your internet connection.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
-    private void checkUserSession(String transport_name, String account_type) {
+    private void checkUserSession(ACProgressFlower dialog, String transport_name, String account_type) {
         if (firebaseAuth.getCurrentUser() != null) {
             usersReference.document(firebaseAuth.getCurrentUser().getUid())
                     .get()
@@ -128,67 +149,90 @@ public class TransportLoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                             if (task.isSuccessful()) {
-                                validateUserInfo(task, transport_name, account_type);
+                                validateUserInfo(dialog, task, transport_name, account_type);
+                            }
+                            else {
+                                dialog.dismiss();
+                                Toast.makeText(TransportLoginActivity.this, "Sign in failed. Please check your internet connection.", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
         }
     }
 
-    private void validateUserInfo(Task<DocumentSnapshot> task, String transport_name, String account_type) {
+    private void validateUserInfo(ACProgressFlower dialog, Task<DocumentSnapshot> task, String transport_name, String account_type) {
         if (transport_name.equals(task.getResult().getString("transport_company"))) {
-            if (account_type.equals(task.getResult().getString("account_type"))) {
+            if (account_type.toLowerCase().equals(task.getResult().getString("account_type"))) {
                 if (account_type.toLowerCase().equals("administrator")) {
-                    startAdminActivity();
+                    partnersReference.whereEqualTo("name", transport_name)
+                            .limit(1)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        String uid = task.getResult().getDocuments().get(0).getString("uid");
+                                        dialog.dismiss();
+                                        startAdminActivity(uid);
+                                    }
+                                    else {
+                                        dialog.dismiss();
+                                        Toast.makeText(TransportLoginActivity.this, "Sign in failed. Please check your internet connection.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 } else {
-                    startUserActivity();
+                    partnersReference.whereEqualTo("name", transport_name)
+                            .limit(1)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        String uid = task.getResult().getDocuments().get(0).getString("uid");
+                                        dialog.dismiss();
+                                        startUserActivity(uid);
+                                    }
+                                    else {
+                                        dialog.dismiss();
+                                        Toast.makeText(TransportLoginActivity.this, "Sign in failed. Please check your internet connection.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
             } else {
                 firebaseAuth.signOut();
                 enableInput();
-                new MaterialAlertDialogBuilder(this)
-                        .setTitle("Sign in failed. Please check your account type and try again.")
-                        .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.dismiss();
-                            }
-                        })
-                        .show();
+                dialog.dismiss();
+                Toast.makeText(this, "Sign in failed. Please check your account type and try again.", Toast.LENGTH_LONG).show();
             }
         } else {
             firebaseAuth.signOut();
             enableInput();
-            new MaterialAlertDialogBuilder(this)
-                    .setTitle("Sign in failed. Please check the name of transport company you belong to and try again.")
-                    .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.dismiss();
-                        }
-                    })
-                    .show();
+            dialog.dismiss();
+            Toast.makeText(this, "Sign in failed. Please check the name of transport company you belong to and try again", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void startAdminActivity() {
-        Intent intent = new Intent(TransportLoginActivity.this, AdminActivity.class);
+    private void startAdminActivity(String uid) {
+        Intent intent = new Intent(TransportLoginActivity.this, TransportCompanyAdminActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        finish();
+        intent.putExtra("uid", uid);
         startActivity(intent);
+        finish();
     }
 
-    private void startUserActivity() {
+    private void startUserActivity(String uid) {
         Intent intent = new Intent(TransportLoginActivity.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        finish();
+        intent.putExtra("uid", uid);
         startActivity(intent);
+        finish();
     }
 
     private void populateTransportNameList() {
         ArrayList<String> transportNameList = new ArrayList<>();
-        usersReference.whereEqualTo("account_type", "transport_company")
-                .get()
+        partnersReference.get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -198,7 +242,7 @@ public class TransportLoginActivity extends AppCompatActivity {
                             }
                             ArrayAdapter<String> transportNameAdapter = new ArrayAdapter<>(TransportLoginActivity.this, R.layout.support_simple_spinner_dropdown_item, transportNameList);
                             inputTransportNameACT.setAdapter(transportNameAdapter);
-                            inputTransportNameACT.setThreshold(2);
+                            inputTransportNameACT.setThreshold(1);
                         }
                     }
                 });
