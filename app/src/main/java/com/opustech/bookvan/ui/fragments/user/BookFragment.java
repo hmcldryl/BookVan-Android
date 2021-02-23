bb0e015a8d46ed271a03b1a442e4fe3b68b38ef9
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-23 17:30:59 +0800

    Update BookFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/fragments/user/BookFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/user/BookFragment.java
@@ -38,6 +38,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.opustech.bookvan.R;
+import com.opustech.bookvan.transport.TransportLoginActivity;
 
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
@@ -56,10 +57,11 @@ public class BookFragment extends Fragment {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
-    private CollectionReference usersReference;
+    private CollectionReference usersReference, bookingsReference, partnersReference;
 
     private TextInputLayout bookingCustomerName,
             bookingContactNumber,
+            bookingVanTransport,
             bookingLocationFrom,
             bookingLocationTo,
             bookingScheduleDate,
@@ -71,6 +73,7 @@ public class BookFragment extends Fragment {
             subtractAdultCount,
             subtractChildCount;
     private Button btnBook;
+    private AutoCompleteTextView inputVanTransportACT, bookingLocationFromACT, bookingLocationToACT;
     private TextView priceTotal;
 
     private String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
@@ -82,13 +85,19 @@ public class BookFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
         usersReference = firebaseFirestore.collection("users");
+        bookingsReference = firebaseFirestore.collection("bookings");
+        partnersReference = firebaseFirestore.collection("partners");
 
         String currentUserId = firebaseAuth.getCurrentUser().getUid();
 
         bookingCustomerName = root.findViewById(R.id.bookingCustomerName);
         bookingContactNumber = root.findViewById(R.id.bookingContactNumber);
+        bookingVanTransport = root.findViewById(R.id.inputVanTransport);
+        inputVanTransportACT = root.findViewById(R.id.inputVanTransportACT);
         bookingLocationFrom = root.findViewById(R.id.bookingLocationFrom);
+        bookingLocationFromACT = root.findViewById(R.id.bookingLocationFromACT);
         bookingLocationTo = root.findViewById(R.id.bookingLocationTo);
+        bookingLocationToACT = root.findViewById(R.id.bookingLocationToACT);
         bookingScheduleDate = root.findViewById(R.id.bookingScheduleDate);
         bookingScheduleTime = root.findViewById(R.id.bookingScheduleTime);
         bookingCountAdult = root.findViewById(R.id.bookingCountAdult);
@@ -104,17 +113,8 @@ public class BookFragment extends Fragment {
         priceTotal = root.findViewById(R.id.price);
         priceTotal.setText("170.00");
 
-        AutoCompleteTextView bookingLocationFromACT = root.findViewById(R.id.bookingLocationFromACT);
-        AutoCompleteTextView bookingLocationToACT = root.findViewById(R.id.bookingLocationToACT);
-
-        ArrayList<String> locationArray = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.destinations)));
-        ArrayAdapter<String> locationArrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, locationArray);
-
-        bookingLocationFromACT.setAdapter(locationArrayAdapter);
-        bookingLocationFromACT.setThreshold(1);
-        bookingLocationToACT.setAdapter(locationArrayAdapter);
-        bookingLocationToACT.setThreshold(1);
-
+        populateVanTransportList();
+        populateLocationList();
         initializeDatePicker();
         initializeTimePicker();
 
@@ -168,7 +168,7 @@ public class BookFragment extends Fragment {
         btnBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnBook.setEnabled(false);
+                disableInput();
                 inputCheck(currentUserId);
             }
         });
@@ -176,7 +176,14 @@ public class BookFragment extends Fragment {
         return root;
     }
 
-    private void generateRefNum(String uid, String name, String contact_number, String location_from, String location_to, String schedule_date, String schedule_time, int count_adult, int count_child, float price) {
+    private void generateRefNum(String uid, String name, String contact_number, String transport_name, String location_from, String location_to, String schedule_date, String schedule_time, int count_adult, int count_child, float price) {
+        final ACProgressFlower dialog = new ACProgressFlower.Builder(getActivity())
+                .direction(ACProgressConstant.DIRECT_CLOCKWISE)
+                .themeColor(getResources().getColor(R.color.colorAccent))
+                .text("Processing...")
+                .fadeColor(Color.DKGRAY).build();
+        dialog.show();
+
         usersReference.document(admin_uid)
                 .collection("bookings")
                 .get()
@@ -186,15 +193,40 @@ public class BookFragment extends Fragment {
                         if (task.isSuccessful()) {
                             int num = task.getResult().getDocuments().size() + 1;
                             String reference_number = "BV-" + String.format(Locale.ENGLISH, "%06d", num);
-                            addNewBooking(reference_number, uid, name, contact_number, location_from, location_to, schedule_date, schedule_time, count_adult, count_child, price);
+                            addNewBooking(dialog, reference_number, uid, name, contact_number, transport_name, location_from, location_to, schedule_date, schedule_time, count_adult, count_child, price);
                         }
                     }
                 });
     }
 
+    private void disableInput() {
+        btnBook.setEnabled(false);
+        bookingCustomerName.setEnabled(false);
+        bookingContactNumber.setEnabled(false);
+        bookingLocationFrom.setEnabled(false);
+        bookingLocationTo.setEnabled(false);
+        bookingScheduleDate.setEnabled(false);
+        bookingScheduleTime.setEnabled(false);
+        bookingCountAdult.setEnabled(false);
+        bookingCountChild.setEnabled(false);
+    }
+
+    private void enableInput() {
+        btnBook.setEnabled(true);
+        bookingCustomerName.setEnabled(true);
+        bookingContactNumber.setEnabled(true);
+        bookingLocationFrom.setEnabled(true);
+        bookingLocationTo.setEnabled(true);
+        bookingScheduleDate.setEnabled(true);
+        bookingScheduleTime.setEnabled(true);
+        bookingCountAdult.setEnabled(true);
+        bookingCountChild.setEnabled(true);
+    }
+
     private void inputCheck(String uid) {
         String name = bookingCustomerName.getEditText().getText().toString();
         String contact_number = bookingContactNumber.getEditText().getText().toString();
+        String transport_name = bookingVanTransport.getEditText().getText().toString();
         String location_from = bookingLocationFrom.getEditText().getText().toString();
         String location_to = bookingLocationTo.getEditText().getText().toString();
         String schedule_date = bookingScheduleDate.getEditText().getText().toString();
@@ -204,28 +236,31 @@ public class BookFragment extends Fragment {
         float price = Float.parseFloat(priceTotal.getText().toString());
 
         if (name.isEmpty()) {
+            enableInput();
             bookingCustomerName.getEditText().setError("Please enter your name.");
-            btnBook.setEnabled(true);
         } else if (contact_number.isEmpty()) {
+            enableInput();
             bookingContactNumber.getEditText().setError("Please enter a contact number.");
-            btnBook.setEnabled(true);
         } else if (location_from.isEmpty()) {
+            enableInput();
             bookingLocationFrom.getEditText().setError("Please enter your starting location.");
-            btnBook.setEnabled(true);
+        } else if (transport_name.isEmpty()) {
+            enableInput();
+            bookingVanTransport.getEditText().setError("Please select your preferred van transport.");
         } else if (location_to.isEmpty()) {
+            enableInput();
             bookingLocationTo.getEditText().setError("Please enter the location you wish to go.");
-            btnBook.setEnabled(true);
         } else if (schedule_date.isEmpty()) {
+            enableInput();
             bookingScheduleDate.getEditText().setError("Please enter desired schedule date.");
-            btnBook.setEnabled(true);
         } else if (schedule_time.isEmpty()) {
+            enableInput();
             bookingScheduleTime.getEditText().setError("Please enter desired schedule time.");
-            btnBook.setEnabled(true);
         } else if (count_adult == 0 && count_child == 0) {
+            enableInput();
             bookingCountAdult.getEditText().setError("Must have at least 1 adult passenger.");
-            btnBook.setEnabled(true);
         } else {
-            generateRefNum(uid, name, contact_number, location_from, location_to, schedule_date, schedule_time, count_adult, count_child, price);
+            generateRefNum(uid, name, contact_number, transport_name, location_from, location_to, schedule_date, schedule_time, count_adult, count_child, price);
         }
     }
 
@@ -234,19 +269,13 @@ public class BookFragment extends Fragment {
         return format.format(Calendar.getInstance().getTime());
     }
 
-    private void addNewBooking(String reference_number, String uid, String name, String contact_number, String location_from, String location_to, String schedule_date, String schedule_time, int count_adult, int count_child, float price) {
-        final ACProgressFlower dialog = new ACProgressFlower.Builder(getActivity())
-                .direction(ACProgressConstant.DIRECT_CLOCKWISE)
-                .themeColor(getResources().getColor(R.color.colorAccent))
-                .text("Processing...")
-                .fadeColor(Color.DKGRAY).build();
-        dialog.show();
-
+    private void addNewBooking(ACProgressFlower dialog, String reference_number, String uid, String name, String contact_number, String transport_name, String location_from, String location_to, String schedule_date, String schedule_time, int count_adult, int count_child, float price) {
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("reference_number", reference_number);
         hashMap.put("uid", uid);
         hashMap.put("name", name);
         hashMap.put("contact_number", contact_number);
+        hashMap.put("transport_name", transport_name);
         hashMap.put("location_from", location_from);
         hashMap.put("location_to", location_to);
         hashMap.put("schedule_date", schedule_date);
@@ -257,26 +286,17 @@ public class BookFragment extends Fragment {
         hashMap.put("timestamp", generateTimestamp());
         hashMap.put("status", "pending");
 
-        usersReference.document(admin_uid)
-                .collection("bookings")
-                .add(hashMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentReference> task) {
-                if (task.isSuccessful()) {
-                    dialog.dismiss();
-                    btnBook.setEnabled(true);
-                    new MaterialAlertDialogBuilder(getActivity())
-                            .setTitle("Booking success!")
-                            .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                            .show();
-                }
-            }
-        });
+        bookingsReference.add(hashMap)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isSuccessful()) {
+                            dialog.dismiss();
+                            enableInput();
+                            Toast.makeText(getActivity(), "Booking success!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void initializeTimePicker() {
@@ -322,5 +342,32 @@ public class BookFragment extends Fragment {
                         calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+    }
+
+    private void populateVanTransportList() {
+        ArrayList<String> vanTransportList = new ArrayList<>();
+        partnersReference.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (int i = 0; i < task.getResult().getDocuments().size(); i++) {
+                                vanTransportList.add(i, task.getResult().getDocuments().get(i).getString("name"));
+                            }
+                            ArrayAdapter<String> vanTransportAdapter = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, vanTransportList);
+                            inputVanTransportACT.setAdapter(vanTransportAdapter);
+                            inputVanTransportACT.setThreshold(1);
+                        }
+                    }
+                });
+    }
+
+    private void populateLocationList() {
+        ArrayList<String> locationArray = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.destinations)));
+        ArrayAdapter<String> locationArrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, locationArray);
+        bookingLocationFromACT.setAdapter(locationArrayAdapter);
+        bookingLocationFromACT.setThreshold(1);
+        bookingLocationToACT.setAdapter(locationArrayAdapter);
+        bookingLocationToACT.setThreshold(1);
     }
 }
