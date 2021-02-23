907416752e12f5efceed259e9e08bdb52e6ec7b2
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-23 17:29:59 +0800

    Create DashboardAdminFragment.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/admin/DashboardAdminFragment.java
@@ -0,0 +1,29 @@
+package com.opustech.bookvan.ui.fragments.admin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.opustech.bookvan.R;
+
+import org.imaginativeworld.whynotimagecarousel.CarouselItem;
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardAdminFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard_admin, container, false);
+
+        
+
+        return root;
+    }
+}
