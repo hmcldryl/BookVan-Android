3b071d458230b59edd3909df6fa5d1ac6e778d18
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-24 01:54:39 +0800

    Create TransportProfileActivity.java

---
+++ app/src/main/java/com/opustech/bookvan/transport/TransportProfileActivity.java
@@ -0,0 +1,16 @@
+package com.opustech.bookvan.transport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.opustech.bookvan.R;
+
+public class TransportProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transport_profile);
+    }
+}
