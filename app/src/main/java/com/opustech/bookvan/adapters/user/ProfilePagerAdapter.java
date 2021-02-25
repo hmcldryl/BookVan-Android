f7c9e8e9e35de1b55f26571bf40ba6b7511ada91
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-25 20:04:42 +0800

    Create ProfilePagerAdapter.java

---
+++ app/src/main/java/com/opustech/bookvan/adapters/user/ProfilePagerAdapter.java
@@ -0,0 +1,32 @@
+package com.opustech.bookvan.adapters.user;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.opustech.bookvan.ui.fragments.user.booking.ActiveBookingFragment;
+import com.opustech.bookvan.ui.fragments.user.booking.BookingHistoryFragment;
+
+public class ProfilePagerAdapter extends FragmentStateAdapter {
+
+    public ProfilePagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new BookingHistoryFragment();
+            default:
+                return new ActiveBookingFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
