83d6c416d33244dc08971bf5c34ed11b8083669c
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-25 20:02:35 +0800

    Create AdapterBookingPendingAdminListRV.java

---
+++ app/src/main/java/com/opustech/bookvan/adapters/admin/AdapterBookingPendingAdminListRV.java
@@ -0,0 +1,175 @@
+package com.opustech.bookvan.adapters.admin;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.opustech.bookvan.R;
+import com.opustech.bookvan.model.Booking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import cc.cloudist.acplibrary.ACProgressConstant;
+import cc.cloudist.acplibrary.ACProgressFlower;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<Booking, AdapterBookingPendingAdminListRV.BookingHolder> {
+
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference usersReference, partnersReference;
+
+    private String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
+
+    private Context context;
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+
+    public AdapterBookingPendingAdminListRV(@NonNull FirestoreRecyclerOptions<Booking> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull BookingHolder holder, int position, @NonNull Booking model) {
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        usersReference = firebaseFirestore.collection("users");
+        partnersReference = firebaseFirestore.collection("partners");
+
+        String uid = model.getUid();
+        String name = model.getName();
+        String contact_number = model.getContact_number();
+        String reference_number = model.getReference_number();
+        String location_from = model.getLocation_from();
+        String location_to = model.getLocation_to();
+        String schedule_date = model.getSchedule_date();
+        String schedule_time = model.getSchedule_time();
+        int count_adult = model.getCount_adult();
+        int count_child = model.getCount_child();
+        String transport_name = model.getTransport_name();
+
+        usersReference.document(uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String customerEmail = task.getResult().getString("email");
+                            holder.bookingCustomerEmail.setText(customerEmail);
+                            String customerPhoto = task.getResult().getString("photo_url");
+                            if (customerPhoto != null) {
+                                if (!customerPhoto.isEmpty()) {
+                                    Glide.with(holder.itemView.getContext())
+                                            .load(customerPhoto)
+                                            .into(holder.customerPhoto);
+                                }
+                            }
+                        }
+                    }
+                });
+
+        holder.bookingCustomerName.setText(name);
+        holder.bookingContactNumber.setText(contact_number);
+        holder.bookingReferenceNumber.setText(reference_number);
+        holder.bookingLocationFrom.setText(location_from);
+        holder.bookingLocationTo.setText(location_to);
+        holder.bookingScheduleDate.setText(schedule_date);
+        holder.bookingScheduleTime.setText(schedule_time);
+        holder.bookingCountAdult.setText(String.valueOf(count_adult));
+        holder.bookingCountChild.setText(String.valueOf(count_child));
+        holder.bookingTransportName.setText(transport_name);
+
+        if (count_adult > 1) {
+            String outputAdult = count_adult + " adults.";
+            holder.bookingCountAdult.setText(outputAdult);
+        } else if (count_adult == 1) {
+            String outputAdult = count_adult + " adult.";
+            holder.bookingCountAdult.setText(outputAdult);
+        } else {
+            holder.bookingCountAdult.setVisibility(View.GONE);
+        }
+
+        if (count_child > 1) {
+            String outputChild = count_child + " children.";
+            holder.bookingCountChild.setText(outputChild);
+        } else if (count_child == 1) {
+            String outputChild = count_child + " child.";
+            holder.bookingCountChild.setText(outputChild);
+        } else {
+            holder.bookingCountChild.setVisibility(View.GONE);
+        }
+    }
+
+    @NonNull
+    @Override
+    public BookingHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_admin_pending_item_layout, parent, false);
+        return new BookingHolder(view);
+    }
+
+    class BookingHolder extends RecyclerView.ViewHolder {
+        TextView bookingCustomerName,
+                bookingCustomerEmail,
+                bookingContactNumber,
+                bookingReferenceNumber,
+                bookingLocationFrom,
+                bookingLocationTo,
+                bookingScheduleDate,
+                bookingScheduleTime,
+                bookingCountAdult,
+                bookingCountChild,
+                bookingTransportName;
+        LinearLayout item;
+        Button btnCancelBooking, btnConfirmBooking;
+        CircleImageView customerPhoto;
+
+        public BookingHolder(View view) {
+            super(view);
+            item = view.findViewById(R.id.item);
+            customerPhoto = view.findViewById(R.id.customerPhoto);
+            btnCancelBooking = view.findViewById(R.id.btnCancelBooking);
+            btnConfirmBooking = view.findViewById(R.id.btnConfirmBooking);
+            bookingCustomerName = view.findViewById(R.id.bookingCustomerName);
+            bookingCustomerEmail = view.findViewById(R.id.bookingCustomerEmail);
+            bookingContactNumber = view.findViewById(R.id.bookingContactNumber);
+            bookingReferenceNumber = view.findViewById(R.id.bookingReferenceNumber);
+            bookingLocationFrom = view.findViewById(R.id.bookingLocationFrom);
+            bookingLocationTo = view.findViewById(R.id.bookingLocationTo);
+            bookingScheduleDate = view.findViewById(R.id.bookingScheduleDate);
+            bookingScheduleTime = view.findViewById(R.id.bookingScheduleTime);
+            bookingCountAdult = view.findViewById(R.id.bookingCountAdult);
+            bookingCountChild = view.findViewById(R.id.bookingCountChild);
+            bookingTransportName = view.findViewById(R.id.bookingTransportName);
+        }
+    }
+}
