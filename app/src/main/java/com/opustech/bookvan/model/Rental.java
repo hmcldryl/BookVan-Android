0fa2533278ddbc7add7b6333778f568c4613d8b5
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-25 20:05:44 +0800

    Update Rental.java

--- app/src/main/java/com/opustech/bookvan/model/Rental.java
+++ app/src/main/java/com/opustech/bookvan/model/Rental.java
@@ -1,21 +1,20 @@
 package com.opustech.bookvan.model;
 
-public class Rental {
-    String owner, van_description, timestamp;
-    String[] photo_url;
-    int capacity;
-    float price;
+import java.util.List;
 
-    public Rental(String owner, String van_description, String timestamp, String[] photo_url, int capacity, float price) {
+public class Rental {
+    String owner, van_description, timestamp, price;
+    List<String> photo_url;
+
+    public Rental() {
+    }
+
+    public Rental(String owner, String van_description, String timestamp, List<String> photo_url, String price) {
         this.owner = owner;
         this.van_description = van_description;
         this.timestamp = timestamp;
         this.photo_url = photo_url;
-        this.capacity = capacity;
         this.price = price;
-    }
-
-    public Rental() {
     }
 
     public String getOwner() {
@@ -42,27 +41,19 @@ public class Rental {
         this.timestamp = timestamp;
     }
 
-    public String[] getPhoto_url() {
+    public List<String> getPhoto_url() {
         return photo_url;
     }
 
-    public void setPhoto_url(String[] photo_url) {
+    public void setPhoto_url(List<String> photo_url) {
         this.photo_url = photo_url;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public float getPrice() {
+    public String getPrice() {
         return price;
     }
 
-    public void setPrice(float price) {
+    public void setPrice(String price) {
         this.price = price;
     }
 }
