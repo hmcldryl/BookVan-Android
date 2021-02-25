00003dfc3ef4ecbe5fa19a3e47133b2c85480cc2
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-02-25 20:03:53 +0800

    Create BookingsTransportPagerAdapter.java

---
+++ app/src/main/java/com/opustech/bookvan/adapters/transport/BookingsTransportPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.opustech.bookvan.adapters.transport;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.opustech.bookvan.ui.fragments.transport.bookings.BookingsConfirmedTransportFragment;
+import com.opustech.bookvan.ui.fragments.transport.bookings.BookingsHistoryTransportFragment;
+import com.opustech.bookvan.ui.fragments.transport.bookings.BookingsPendingTransportFragment;
+
+public class BookingsTransportPagerAdapter extends FragmentStateAdapter {
+
+    public BookingsTransportPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new BookingsPendingTransportFragment();
+            case 2:
+                return new BookingsHistoryTransportFragment();
+            default:
+                return new BookingsConfirmedTransportFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
