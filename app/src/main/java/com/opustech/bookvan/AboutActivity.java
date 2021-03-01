1feb9f28f5045428ae8ad9fa67a97a2542d7a44c
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-03-01 19:00:53 +0800

    Update AboutActivity.java

--- app/src/main/java/com/opustech/bookvan/AboutActivity.java
+++ app/src/main/java/com/opustech/bookvan/AboutActivity.java
@@ -1,14 +1,82 @@
 package com.opustech.bookvan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.imaginativeworld.whynotimagecarousel.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private DocumentReference systemReference;
+
+    private TextView textAppDescription;
+    private Button btnLicenses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        systemReference = firebaseFirestore.collection("system").document("data");
+
+        textAppDescription = findViewById(R.id.app_description);
+        btnLicenses = findViewById(R.id.btnLicenses);
+
+        updateUi();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("About BookVan");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        btnLicenses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutActivity.this, OssLicensesMenuActivity.class));
+            }
+        });
+    }
+
+    private void updateUi() {
+        systemReference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String app_description = task.getResult().getString("app_description");
+                            if (app_description != null) {
+                                textAppDescription.setText(app_description);
+                            }
+                        }
+                    }
+                });
     }
 }
