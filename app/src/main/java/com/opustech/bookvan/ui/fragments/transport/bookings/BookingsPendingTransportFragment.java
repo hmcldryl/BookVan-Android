5c4eecae0ecdc9fe6ddb1d9733b6bd6475ec58da
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-03-01 19:11:37 +0800

    Update BookingsPendingTransportFragment.java

--- app/src/main/java/com/opustech/bookvan/ui/fragments/transport/bookings/BookingsPendingTransportFragment.java
+++ app/src/main/java/com/opustech/bookvan/ui/fragments/transport/bookings/BookingsPendingTransportFragment.java
@@ -11,15 +11,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -31,41 +28,24 @@ import com.opustech.bookvan.adapters.transport.AdapterBookingPendingTransportLis
 
 public class BookingsPendingTransportFragment extends Fragment {
 
-    private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
-    private CollectionReference usersReference, bookingsReference, partnersReference;
+    private CollectionReference bookingsReference, partnersReference;
 
     private TextView bookingStatusNone;
     private RecyclerView bookingList;
 
     private AdapterBookingPendingTransportListRV adapterBookingPendingTransportListRV;
 
-    private String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_bookings_pending_transport, container, false);
 
-        firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
-        usersReference = firebaseFirestore.collection("users");
         bookingsReference = firebaseFirestore.collection("bookings");
         partnersReference = firebaseFirestore.collection("partners");
 
-        //currentUserID = firebaseAuth.getCurrentUser().getUid();
-
-        partnersReference.document(getCompanyUid())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            String transport_name = task.getResult().getString("name");
-                            populateList(root, transport_name);
-                            updateUi(transport_name);
-                        }
-                    }
-                });
+        populateList(root, getTransportName());
+        updateUi(getTransportName());
 
         return root;
     }
@@ -88,12 +68,24 @@ public class BookingsPendingTransportFragment extends Fragment {
                         }
                     }
                 });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapterBookingPendingTransportListRV.cancelBooking(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(bookingList);
     }
 
     private void populateList(View root, String transport_name) {
         Query query = bookingsReference.whereEqualTo("transport_name", transport_name)
                 .whereEqualTo("status", "pending")
-                .orderBy("timestamp", Query.Direction.DESCENDING);
+                .orderBy("timestamp", Query.Direction.ASCENDING);
 
         FirestoreRecyclerOptions<Booking> options = new FirestoreRecyclerOptions.Builder<Booking>()
                 .setQuery(query, Booking.class)
@@ -115,6 +107,11 @@ public class BookingsPendingTransportFragment extends Fragment {
     private String getCompanyUid() {
         Intent intent = getActivity().getIntent();
         return intent.getStringExtra("uid");
+    }
+
+    private String getTransportName() {
+        Intent intent = getActivity().getIntent();
+        return intent.getStringExtra("transport_name");
     }
 
     @Override
