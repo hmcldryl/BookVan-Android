5623d67a21421c0d5c154faf543fc762b7760778
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-09 20:02:50 +0800

    Update AdapterBookingPendingAdminListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/admin/AdapterBookingPendingAdminListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/admin/AdapterBookingPendingAdminListRV.java
@@ -70,7 +70,7 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
         String schedule_time = model.getSchedule_time();
         int count_adult = model.getCount_adult();
         int count_child = model.getCount_child();
-        String transport_name = model.getTransport_name();
+        String transport_uid = model.getTransport_uid();
 
         usersReference.document(uid)
                 .get()
@@ -92,6 +92,18 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
                     }
                 });
 
+        partnersReference.document(transport_uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String transport_name = task.getResult().getString("transport_name");
+                            holder.bookingTransportName.setText(transport_name);
+                        }
+                    }
+                });
+
         holder.bookingCustomerName.setText(name);
         holder.bookingContactNumber.setText(contact_number);
         holder.bookingReferenceNumber.setText(reference_number);
@@ -101,7 +113,6 @@ public class AdapterBookingPendingAdminListRV extends FirestoreRecyclerAdapter<B
         holder.bookingScheduleTime.setText(schedule_time);
         holder.bookingCountAdult.setText(String.valueOf(count_adult));
         holder.bookingCountChild.setText(String.valueOf(count_child));
-        holder.bookingTransportName.setText(transport_name);
 
         if (count_adult > 1) {
             String outputAdult = count_adult + " adults.";
