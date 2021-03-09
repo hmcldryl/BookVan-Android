72a7bf9bfd8a498fd25de68f1656534059cd7de1
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-09 20:03:02 +0800

    Update AdapterBookingConfirmedListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/user/AdapterBookingConfirmedListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/user/AdapterBookingConfirmedListRV.java
@@ -34,7 +34,7 @@ import se.simbio.encryption.Encryption;
 public class AdapterBookingConfirmedListRV extends FirestoreRecyclerAdapter<Booking, AdapterBookingConfirmedListRV.BookingHolder> {
 
     private FirebaseFirestore firebaseFirestore;
-    private CollectionReference usersReference;
+    private CollectionReference usersReference, partnersReference;
 
     private final String admin_uid = "yEali5UosERXD1wizeJGN87ffff2";
 
@@ -59,6 +59,7 @@ public class AdapterBookingConfirmedListRV extends FirestoreRecyclerAdapter<Book
     protected void onBindViewHolder(@NonNull BookingHolder holder, int position, @NonNull Booking model) {
         firebaseFirestore = FirebaseFirestore.getInstance();
         usersReference = firebaseFirestore.collection("users");
+        partnersReference = firebaseFirestore.collection("partners");
 
         String uid = model.getUid();
         String name = model.getName();
@@ -70,7 +71,7 @@ public class AdapterBookingConfirmedListRV extends FirestoreRecyclerAdapter<Book
         String schedule_time = model.getSchedule_time();
         int count_adult = model.getCount_adult();
         int count_child = model.getCount_child();
-        String transport_name = model.getTransport_name();
+        String transport_uid = model.getTransport_uid();
         String driver_name = model.getDriver_name();
         String plate_number = model.getPlate_number();
         float price = model.getPrice();
@@ -93,6 +94,18 @@ public class AdapterBookingConfirmedListRV extends FirestoreRecyclerAdapter<Book
                     }
                 });
 
+        partnersReference.document(transport_uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String transport_name = task.getResult().getString("transport_name");
+                            holder.bookingTransportName.setText(transport_name);
+                        }
+                    }
+                });
+
         holder.bookingCustomerName.setText(name);
         holder.bookingContactNumber.setText(contact_number);
         holder.bookingReferenceNumber.setText(reference_number);
@@ -100,7 +113,6 @@ public class AdapterBookingConfirmedListRV extends FirestoreRecyclerAdapter<Book
         holder.bookingLocationTo.setText(location_to);
         holder.bookingScheduleDate.setText(schedule_date);
         holder.bookingScheduleTime.setText(schedule_time);
-        holder.bookingTransportName.setText(transport_name);
         holder.bookingDriverName.setText(driver_name);
         holder.bookingPlateNumber.setText(plate_number);
 
