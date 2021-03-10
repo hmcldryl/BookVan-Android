365fbd875cb5e612e379c538e94f3c7351f63b37
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-10 20:29:01 +0800

    Update AdapterBookingHistoryAdminListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/admin/AdapterBookingHistoryAdminListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/admin/AdapterBookingHistoryAdminListRV.java
@@ -44,6 +44,8 @@ public class AdapterBookingHistoryAdminListRV extends FirestoreRecyclerAdapter<B
 
     @Override
     protected void onBindViewHolder(@NonNull BookingHolder holder, int position, @NonNull Booking model) {
+        holder.itemNumber.setText(String.valueOf(position + 1));
+
         firebaseFirestore = FirebaseFirestore.getInstance();
         usersReference = firebaseFirestore.collection("users");
         partnersReference = firebaseFirestore.collection("partners");
@@ -87,7 +89,7 @@ public class AdapterBookingHistoryAdminListRV extends FirestoreRecyclerAdapter<B
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
-                            String transport_name = task.getResult().getString("transport_name");
+                            String transport_name = task.getResult().getString("name");
                             holder.bookingTransportName.setText(transport_name);
                         }
                     }
@@ -143,13 +145,15 @@ public class AdapterBookingHistoryAdminListRV extends FirestoreRecyclerAdapter<B
                 bookingPlateNumber,
                 bookingPrice,
                 labelCountAdult,
-                labelCountChild;
+                labelCountChild,
+                itemNumber;
         LinearLayout item;
         CircleImageView customerPhoto;
 
         public BookingHolder(View view) {
             super(view);
             item = view.findViewById(R.id.item);
+            itemNumber = view.findViewById(R.id.itemNumber);
             customerPhoto = view.findViewById(R.id.customerPhoto);
             bookingCustomerName = view.findViewById(R.id.bookingCustomerName);
             bookingCustomerEmail = view.findViewById(R.id.bookingCustomerEmail);
