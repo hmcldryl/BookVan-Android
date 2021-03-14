dc89f485e6d931000ddf82f23367ee9c7a1c8306
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-14 20:09:39 +0800

    Update TransportAdminSchedulesActivity.java

--- app/src/main/java/com/opustech/bookvan/ui/transport/TransportAdminSchedulesActivity.java
+++ app/src/main/java/com/opustech/bookvan/ui/transport/TransportAdminSchedulesActivity.java
@@ -1,8 +1,10 @@
 package com.opustech.bookvan.ui.transport;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.opustech.bookvan.R;
 
@@ -12,5 +14,19 @@ public class TransportAdminSchedulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport_admin_schedules);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("Schedules");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
     }
 }
