1db548fdc283245bef034c5164e32e03e8d400e7
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-15 20:16:40 +0800

    Update Rental.java

--- app/src/main/java/com/opustech/bookvan/model/Rental.java
+++ app/src/main/java/com/opustech/bookvan/model/Rental.java
@@ -1,46 +1,94 @@
 package com.opustech.bookvan.model;
 
-import java.util.List;
-
 public class Rental {
-    String van_model, owner, van_description, timestamp;
-    float price;
-    List<String> photo_url;
+    String uid, rent_type, name, location_pickup, location_dropoff, contact_number, destination, schedule_start_date, schedule_end_date, timestamp;
 
     public Rental() {
     }
 
-    public Rental(String van_model, String owner, String van_description, String timestamp, float price, List<String> photo_url) {
-        this.van_model = van_model;
-        this.owner = owner;
-        this.van_description = van_description;
+    public Rental(String uid, String rent_type, String name, String location_pickup, String location_dropoff, String contact_number, String destination, String schedule_start_date, String schedule_end_date, String timestamp) {
+        this.uid = uid;
+        this.rent_type = rent_type;
+        this.name = name;
+        this.location_pickup = location_pickup;
+        this.location_dropoff = location_dropoff;
+        this.contact_number = contact_number;
+        this.destination = destination;
+        this.schedule_start_date = schedule_start_date;
+        this.schedule_end_date = schedule_end_date;
         this.timestamp = timestamp;
-        this.price = price;
-        this.photo_url = photo_url;
     }
 
-    public String getVan_model() {
-        return van_model;
+    public String getUid() {
+        return uid;
     }
 
-    public void setVan_model(String van_model) {
-        this.van_model = van_model;
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 
-    public String getOwner() {
-        return owner;
+    public String getRent_type() {
+        return rent_type;
     }
 
-    public void setOwner(String owner) {
-        this.owner = owner;
+    public void setRent_type(String rent_type) {
+        this.rent_type = rent_type;
     }
 
-    public String getVan_description() {
-        return van_description;
+    public String getName() {
+        return name;
     }
 
-    public void setVan_description(String van_description) {
-        this.van_description = van_description;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation_pickup() {
+        return location_pickup;
+    }
+
+    public void setLocation_pickup(String location_pickup) {
+        this.location_pickup = location_pickup;
+    }
+
+    public String getLocation_dropoff() {
+        return location_dropoff;
+    }
+
+    public void setLocation_dropoff(String location_dropoff) {
+        this.location_dropoff = location_dropoff;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getSchedule_start_date() {
+        return schedule_start_date;
+    }
+
+    public void setSchedule_start_date(String schedule_start_date) {
+        this.schedule_start_date = schedule_start_date;
+    }
+
+    public String getSchedule_end_date() {
+        return schedule_end_date;
+    }
+
+    public void setSchedule_end_date(String schedule_end_date) {
+        this.schedule_end_date = schedule_end_date;
     }
 
     public String getTimestamp() {
@@ -49,21 +97,5 @@ public class Rental {
 
     public void setTimestamp(String timestamp) {
         this.timestamp = timestamp;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public List<String> getPhoto_url() {
-        return photo_url;
-    }
-
-    public void setPhoto_url(List<String> photo_url) {
-        this.photo_url = photo_url;
     }
 }
