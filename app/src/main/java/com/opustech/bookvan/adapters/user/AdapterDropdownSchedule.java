2f2a3fd54da1e63f05714654ede66f061dcae510
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-15 20:16:26 +0800

    Create AdapterDropdownSchedule.java

---
+++ app/src/main/java/com/opustech/bookvan/adapters/user/AdapterDropdownSchedule.java
@@ -0,0 +1,49 @@
+package com.opustech.bookvan.adapters.user;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.opustech.bookvan.R;
+import com.opustech.bookvan.model.Schedule;
+
+import java.util.ArrayList;
+
+public class AdapterDropdownSchedule extends ArrayAdapter<Schedule> {
+    public AdapterDropdownSchedule(@NonNull Context context, ArrayList<Schedule> scheduleArrayList) {
+        super(context, 0, scheduleArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    private View initView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.route_spinner_dropdown_item, parent, false);
+        }
+        TextView routeDescription = convertView.findViewById(R.id.routeDescription);
+        TextView routePrice = convertView.findViewById(R.id.routePrice);
+        Schedule schedule = getItem(position);
+        if (schedule != null) {
+            String route = schedule.getRoute_from() + " - " + schedule.getRoute_to();
+            routeDescription.setText(route);
+            routePrice.setText(String.valueOf(schedule.getPrice()));
+        }
+        return convertView;
+
+    }
+}
