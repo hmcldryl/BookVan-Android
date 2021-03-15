90049d73d3d34c3d1a7fc7fb6067212ea7b9b02b
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-15 20:16:11 +0800

    Update AdapterBookingHistoryTransportListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingHistoryTransportListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingHistoryTransportListRV.java
@@ -55,8 +55,7 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
         String name = model.getName();
         String contact_number = model.getContact_number();
         String reference_number = model.getReference_number();
-        String location_from = model.getLocation_from();
-        String location_to = model.getLocation_to();
+        String trip_route = model.getTrip_route();
         String schedule_date = model.getSchedule_date();
         String schedule_time = model.getSchedule_time();
         int count_adult = model.getCount_adult();
@@ -99,8 +98,7 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
         holder.bookingCustomerName.setText(name);
         holder.bookingContactNumber.setText(contact_number);
         holder.bookingReferenceNumber.setText(reference_number);
-        holder.bookingLocationFrom.setText(location_from);
-        holder.bookingLocationTo.setText(location_to);
+        holder.bookingTripRoute.setText(trip_route);
         holder.bookingScheduleDate.setText(schedule_date);
         holder.bookingScheduleTime.setText(schedule_time);
         holder.bookingDriverName.setText(driver_name);
@@ -135,8 +133,7 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
                 bookingCustomerEmail,
                 bookingContactNumber,
                 bookingReferenceNumber,
-                bookingLocationFrom,
-                bookingLocationTo,
+                bookingTripRoute,
                 bookingScheduleDate,
                 bookingScheduleTime,
                 bookingCountAdult,
@@ -160,8 +157,7 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
             bookingCustomerEmail = view.findViewById(R.id.bookingCustomerEmail);
             bookingContactNumber = view.findViewById(R.id.bookingContactNumber);
             bookingReferenceNumber = view.findViewById(R.id.bookingReferenceNumber);
-            bookingLocationFrom = view.findViewById(R.id.bookingLocationFrom);
-            bookingLocationTo = view.findViewById(R.id.bookingLocationTo);
+            bookingTripRoute = view.findViewById(R.id.bookingTripRoute);
             bookingScheduleDate = view.findViewById(R.id.bookingScheduleDate);
             bookingScheduleTime = view.findViewById(R.id.bookingScheduleTime);
             bookingCountAdult = view.findViewById(R.id.bookingCountAdult);
