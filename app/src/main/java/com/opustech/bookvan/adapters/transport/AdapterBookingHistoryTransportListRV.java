eb5a3fa98b16ef8f0ea6aec1f79d474595342120
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-17 16:33:19 +0800

    Update AdapterBookingHistoryTransportListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingHistoryTransportListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingHistoryTransportListRV.java
@@ -63,7 +63,7 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
         String transport_uid = model.getTransport_uid();
         String driver_name = model.getDriver_name();
         String plate_number = model.getPlate_number();
-        float price = model.getPrice();
+        double price = model.getPrice();
 
         usersReference.document(uid)
                 .get()
@@ -124,7 +124,7 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
     @NonNull
     @Override
     public BookingHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_admin_history_item_layout, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_transport_history_item_layout, parent, false);
         return new BookingHolder(view);
     }
 
