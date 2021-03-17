9746ed1974dd940b0ddf58cb4aa688ee755f4876
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-17 16:33:21 +0800

    Update AdapterBookingPendingTransportListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingPendingTransportListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingPendingTransportListRV.java
@@ -65,36 +65,16 @@ public class AdapterBookingPendingTransportListRV extends FirestoreRecyclerAdapt
         usersReference = firebaseFirestore.collection("users");
         partnersReference = firebaseFirestore.collection("partners");
 
-        String customer_uid = model.getUid();
         String name = model.getName();
-        String contact_number = model.getContact_number();
         String reference_number = model.getReference_number();
         String trip_route = model.getTrip_route();
         String schedule_date = model.getSchedule_date();
         String schedule_time = model.getSchedule_time();
         int count_adult = model.getCount_adult();
         int count_child = model.getCount_child();
+        int count_special = model.getCount_special();
         String transport_uid = model.getTransport_uid();
-
-        usersReference.document(customer_uid)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            String customerEmail = task.getResult().getString("email");
-                            holder.bookingCustomerEmail.setText(customerEmail);
-                            String customerPhoto = task.getResult().getString("photo_url");
-                            if (customerPhoto != null) {
-                                if (!customerPhoto.isEmpty()) {
-                                    Glide.with(context)
-                                            .load(customerPhoto)
-                                            .into(holder.customerPhoto);
-                                }
-                            }
-                        }
-                    }
-                });
+        double price = model.getPrice();
 
         partnersReference.document(transport_uid)
                 .get()
@@ -109,7 +89,6 @@ public class AdapterBookingPendingTransportListRV extends FirestoreRecyclerAdapt
                 });
 
         holder.bookingCustomerName.setText(name);
-        holder.bookingContactNumber.setText(contact_number);
         holder.bookingReferenceNumber.setText(reference_number);
         holder.bookingTripRoute.setText(trip_route);
         holder.bookingScheduleDate.setText(schedule_date);
@@ -138,6 +117,15 @@ public class AdapterBookingPendingTransportListRV extends FirestoreRecyclerAdapt
             holder.bookingCountChild.setVisibility(View.GONE);
             holder.labelCountChild.setVisibility(View.GONE);
         }
+
+        if (count_special >= 1) {
+            String outputSpecial = count_special + " PWD/Senior/Student.";
+            holder.bookingCountSpecial.setText(outputSpecial);
+        } else {
+            holder.bookingCountSpecial.setVisibility(View.GONE);
+        }
+
+        holder.bookingPrice.setText(String.valueOf(price));
 
         holder.item.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -253,23 +241,24 @@ public class AdapterBookingPendingTransportListRV extends FirestoreRecyclerAdapt
     @NonNull
     @Override
     public BookingHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_admin_pending_item_layout, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_transport_pending_item_layout, parent, false);
         return new BookingHolder(view);
     }
 
     class BookingHolder extends RecyclerView.ViewHolder {
         TextView bookingCustomerName,
-                bookingCustomerEmail,
-                bookingContactNumber,
                 bookingReferenceNumber,
                 bookingTripRoute,
                 bookingScheduleDate,
                 bookingScheduleTime,
                 bookingCountAdult,
                 bookingCountChild,
+                bookingCountSpecial,
                 bookingTransportName,
                 labelCountAdult,
                 labelCountChild,
+                labelCountSpecial,
+                bookingPrice,
                 itemNumber;
         LinearLayout item;
         CircleImageView customerPhoto;
@@ -280,8 +269,6 @@ public class AdapterBookingPendingTransportListRV extends FirestoreRecyclerAdapt
             itemNumber = view.findViewById(R.id.itemNumber);
             customerPhoto = view.findViewById(R.id.customerPhoto);
             bookingCustomerName = view.findViewById(R.id.bookingCustomerName);
-            bookingCustomerEmail = view.findViewById(R.id.bookingCustomerEmail);
-            bookingContactNumber = view.findViewById(R.id.bookingContactNumber);
             bookingReferenceNumber = view.findViewById(R.id.bookingReferenceNumber);
             bookingTripRoute = view.findViewById(R.id.bookingTripRoute);
             bookingScheduleDate = view.findViewById(R.id.bookingScheduleDate);
@@ -290,7 +277,10 @@ public class AdapterBookingPendingTransportListRV extends FirestoreRecyclerAdapt
             labelCountAdult = view.findViewById(R.id.labelCountAdult);
             bookingCountChild = view.findViewById(R.id.bookingCountChild);
             labelCountChild = view.findViewById(R.id.labelCountChild);
+            bookingCountSpecial = view.findViewById(R.id.bookingCountSpecial);
+            labelCountSpecial = view.findViewById(R.id.labelCountSpecial);
             bookingTransportName = view.findViewById(R.id.bookingTransportName);
+            bookingPrice = view.findViewById(R.id.price);
         }
     }
 }
