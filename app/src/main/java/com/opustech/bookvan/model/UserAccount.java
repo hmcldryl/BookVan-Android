efd6bd2b65afbdaff4026aef6afe80b6893e3bf1
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-19 18:08:18 +0800

    Create UserAccount.java

---
+++ app/src/main/java/com/opustech/bookvan/model/UserAccount.java
@@ -0,0 +1,45 @@
+package com.opustech.bookvan.model;
+
+public class UserAccount {
+    String name, email, contact_number;
+    int points;
+
+    public UserAccount(String name, String email, String contact_number, int points) {
+        this.name = name;
+        this.email = email;
+        this.contact_number = contact_number;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
