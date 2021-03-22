7ce6676a8fe413434b83f20109bb4c653cd4f64a
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-22 19:25:32 +0800

    Create TransportAdminBookingQRActivity.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/transport/TransportAdminBookingQRActivity.java
@@ -0,0 +1,16 @@
+package com.opustech.bookvan.ui.transport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.opustech.bookvan.R;
+
+public class TransportAdminBookingQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transport_admin_booking_qr);
+    }
+}
