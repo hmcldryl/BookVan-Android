b960852f20ce1044d7f22dfe15b697049a4f92bf
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-22 19:25:24 +0800

    Update Rental.java

--- app/src/main/java/com/opustech/bookvan/model/Rental.java
+++ app/src/main/java/com/opustech/bookvan/model/Rental.java
@@ -1,101 +1,36 @@
 package com.opustech.bookvan.model;
 
 public class Rental {
-    String uid, rent_type, name, location_pickup, location_dropoff, contact_number, destination, schedule_start_date, schedule_end_date, timestamp;
+    String uid,
+            rent_type,
+            name,
+            contact_number,
+            pickup_location,
+            pickup_date,
+            pickup_time,
+            destination,
+            dropoff_location,
+            dropoff_date,
+            dropoff_time,
+            timestamp;
 
     public Rental() {
     }
 
-    public Rental(String uid, String rent_type, String name, String location_pickup, String location_dropoff, String contact_number, String destination, String schedule_start_date, String schedule_end_date, String timestamp) {
+    public Rental(String uid, String rent_type, String name, String contact_number, String pickup_location, String pickup_date, String pickup_time, String destination, String dropoff_location, String dropoff_date, String dropoff_time, String timestamp) {
         this.uid = uid;
         this.rent_type = rent_type;
         this.name = name;
-        this.location_pickup = location_pickup;
-        this.location_dropoff = location_dropoff;
         this.contact_number = contact_number;
+        this.pickup_location = pickup_location;
+        this.pickup_date = pickup_date;
+        this.pickup_time = pickup_time;
         this.destination = destination;
-        this.schedule_start_date = schedule_start_date;
-        this.schedule_end_date = schedule_end_date;
+        this.dropoff_location = dropoff_location;
+        this.dropoff_date = dropoff_date;
+        this.dropoff_time = dropoff_time;
         this.timestamp = timestamp;
     }
 
-    public String getUid() {
-        return uid;
-    }
 
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getRent_type() {
-        return rent_type;
-    }
-
-    public void setRent_type(String rent_type) {
-        this.rent_type = rent_type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLocation_pickup() {
-        return location_pickup;
-    }
-
-    public void setLocation_pickup(String location_pickup) {
-        this.location_pickup = location_pickup;
-    }
-
-    public String getLocation_dropoff() {
-        return location_dropoff;
-    }
-
-    public void setLocation_dropoff(String location_dropoff) {
-        this.location_dropoff = location_dropoff;
-    }
-
-    public String getContact_number() {
-        return contact_number;
-    }
-
-    public void setContact_number(String contact_number) {
-        this.contact_number = contact_number;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getSchedule_start_date() {
-        return schedule_start_date;
-    }
-
-    public void setSchedule_start_date(String schedule_start_date) {
-        this.schedule_start_date = schedule_start_date;
-    }
-
-    public String getSchedule_end_date() {
-        return schedule_end_date;
-    }
-
-    public void setSchedule_end_date(String schedule_end_date) {
-        this.schedule_end_date = schedule_end_date;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
 }
