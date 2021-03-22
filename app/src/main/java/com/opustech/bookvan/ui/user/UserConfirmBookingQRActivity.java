6e3eb11bdbfc0a361bf1fb8ebdcff29d0a690c24
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-03-22 19:25:46 +0800

    Create UserConfirmBookingQRActivity.java

---
+++ app/src/main/java/com/opustech/bookvan/ui/user/UserConfirmBookingQRActivity.java
@@ -0,0 +1,15 @@
+package com.opustech.bookvan.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.opustech.bookvan.R;
+
+public class UserConfirmBookingQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_confirm_booking_qr);
+    }
+}
