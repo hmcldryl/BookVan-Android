ce74e301c1c2c9ed2cfba5e6b1dc3623a34bdc2c
John Daryl Homecillo <daryl.homecillo@gmail.com>
2021-03-23 11:50:53 +0800

    Update UserConfirmBookingQRActivity.java

--- app/src/main/java/com/opustech/bookvan/ui/user/UserConfirmBookingQRActivity.java
+++ app/src/main/java/com/opustech/bookvan/ui/user/UserConfirmBookingQRActivity.java
@@ -1,15 +1,57 @@
 package com.opustech.bookvan.ui.user;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.opustech.bookvan.R;
 
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import se.simbio.encryption.Encryption;
+
 public class UserConfirmBookingQRActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference usersReference, bookingsReference;
+
+    private final String OT_KEY = "TzA8gEdNHRphj6Hu";
+    private final String OT_SALT = "TzA8gEdNHRphj6Hu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_confirm_booking_qr);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        usersReference = firebaseFirestore.collection("users");
+        bookingsReference = firebaseFirestore.collection("bookings");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("Confirm Payment");
+        getSupportActionBar().setSubtitle("Present payment and scan QR to confirm payment.");
+
+        ImageView qrPlaceholder = findViewById(R.id.qrPlaceholder);
+
+        String inputValue = encryptString(getIntent().getStringExtra("reference_number"));
+
+        QRGEncoder qrgEncoder = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, 500);
+        qrgEncoder.setColorBlack(getResources().getColor(R.color.colorPrimary));
+        qrgEncoder.setColorWhite(getResources().getColor(R.color.white));
+        Bitmap bitmap = qrgEncoder.getBitmap();
+        qrPlaceholder.setImageBitmap(bitmap);
+    }
+
+    private String encryptString(String data) {
+        byte[] iv = new byte[16];
+        Encryption encryption = Encryption.getDefault(OT_KEY, OT_SALT, iv);
+        return encryption.encryptOrNull(data);
     }
 }
