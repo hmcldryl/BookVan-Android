e4713ba360a47f2cc0bb0c56aeef9fab862364de
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-04-06 12:58:17 +0800

    Update AdapterBookingHistoryTransportListRV.java

--- app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingHistoryTransportListRV.java
+++ app/src/main/java/com/opustech/bookvan/adapters/transport/AdapterBookingHistoryTransportListRV.java
@@ -117,9 +117,13 @@ public class AdapterBookingHistoryTransportListRV extends FirestoreRecyclerAdapt
             holder.bookingPlateNumber.setText(plate_number);
         }
 
-        if (remarks.isEmpty()) {
-            holder.labelRemarks.setVisibility(View.GONE);
-            holder.bookingRemarks.setVisibility(View.GONE);
+        if (remarks != null) {
+            if (remarks.isEmpty()) {
+                holder.labelRemarks.setVisibility(View.GONE);
+                holder.bookingRemarks.setVisibility(View.GONE);
+            } else {
+                holder.bookingRemarks.setText(remarks);
+            }
         } else {
             holder.bookingRemarks.setText(remarks);
         }
