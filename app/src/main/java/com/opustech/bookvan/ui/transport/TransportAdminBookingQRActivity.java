4f4f57d451d817c1bcac91b3bcc68321d6c2729c
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2021-04-06 12:58:47 +0800

    Update TransportAdminBookingQRActivity.java

--- app/src/main/java/com/opustech/bookvan/ui/transport/TransportAdminBookingQRActivity.java
+++ app/src/main/java/com/opustech/bookvan/ui/transport/TransportAdminBookingQRActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.github.sumimakito.awesomeqr.AwesomeQrRenderer;
+import com.github.sumimakito.awesomeqr.RenderResult;
+import com.github.sumimakito.awesomeqr.option.RenderOption;
+import com.github.sumimakito.awesomeqr.option.color.Color;
+import com.github.sumimakito.awesomeqr.option.logo.Logo;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.opustech.bookvan.R;
 
 import androidmads.library.qrgenearator.QRGContents;
@@ -17,9 +26,6 @@ import androidmads.library.qrgenearator.QRGEncoder;
 import se.simbio.encryption.Encryption;
 
 public class TransportAdminBookingQRActivity extends AppCompatActivity {
-
-    private FirebaseFirestore firebaseFirestore;
-    private CollectionReference usersReference, bookingsReference;
 
     private ImageView qrPlaceholder;
 
@@ -30,10 +36,6 @@ public class TransportAdminBookingQRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport_admin_booking_qr);
-
-        firebaseFirestore = FirebaseFirestore.getInstance();
-        usersReference = firebaseFirestore.collection("users");
-        bookingsReference = firebaseFirestore.collection("bookings");
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -50,19 +52,48 @@ public class TransportAdminBookingQRActivity extends AppCompatActivity {
         });
 
         qrPlaceholder = findViewById(R.id.qrPlaceholder);
-        qrPlaceholder.setImageBitmap(generateQR());
-    }
 
-    private Bitmap generateQR() {
-        QRGEncoder qrgEncoder = new QRGEncoder(encryptString(getIntent().getStringExtra("uid")), null, QRGContents.Type.TEXT, 500);
-        qrgEncoder.setColorBlack(getResources().getColor(R.color.white));
-        qrgEncoder.setColorWhite(getResources().getColor(R.color.colorQRBG));
-        return qrgEncoder.getBitmap();
+        try {
+            RenderResult result = AwesomeQrRenderer.render(renderQR());
+            qrPlaceholder.setImageBitmap(result.getBitmap());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private String encryptString(String data) {
         byte[] iv = new byte[16];
         Encryption encryption = Encryption.getDefault(OT_KEY, OT_SALT, iv);
         return encryption.encryptOrNull(data);
+    }
+
+    private RenderOption renderQR() {
+        RenderOption renderOption = new RenderOption();
+        renderOption.setContent(encryptString(getIntent().getStringExtra("uid"))); // content to encode
+        renderOption.setSize(500);
+        renderOption.setRoundedPatterns(true);
+        renderOption.setClearBorder(true);
+        renderOption.setBorderWidth(0);
+        renderOption.setColor(loadColor());
+        renderOption.setLogo(loadLogo());
+        return renderOption;
+    }
+
+    private Color loadColor() {
+        Color color = new Color();
+        color.setDark(getResources().getColor(R.color.colorAccent));
+        color.setLight(getResources().getColor(R.color.white));
+        color.setBackground(getResources().getColor(R.color.colorPrimary));
+        return color;
+    }
+
+    private Logo loadLogo() {
+        Logo logo = new Logo();
+        logo.setBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.qr_logo));
+        logo.setBorderRadius(10); // radius for logo's corners
+        logo.setBorderWidth(10); // width of the border to be added around the logo
+        logo.setScale(0.3f); // scale for the logo in the QR code
+        logo.setClippingRect(new RectF(0, 0, 0, 0)); // crop the logo image before applying it to the QR code
+        return logo;
     }
 }
